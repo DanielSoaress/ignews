5dea04c9d18a76904a51928dd0df378323b1c08a
Daniel Soares <danielsoaresf@hotmail.com>
2022-12-02 08:22:38 -0300

    bugfix/ Correção .example.
    Retirado variaveis não utilizadas.

--- env.js
+++
@@ -1,4 +0,0 @@
-export const env = {
-    PRISMIC_ACCESS_TOKEN: 'MC5ZbE4tS3hNQUFDUUFFd2d4.77-977-9bO-_vX0qfe-_ve-_ve-_vT4FAe-_ve-_ve-_ve-_vRnvv719Jy4iSe-_vTFG77-9cnNM77-9',
-    PRISMIC_END_PONT: 'https://ignews-lost.prismic.io/api/v2',
-}
