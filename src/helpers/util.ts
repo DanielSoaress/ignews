7877ea33c5b19173d6504b974d04bcc342f6535b
Daniel Soares <danielsoaresf@hotmail.com>
2022-12-03 13:04:45 -0300

    refactor/ Refatorando tela do post.

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -2,11 +2,11 @@ import { RichText } from 'prismic-dom';
 
 export const dateToPtbr = (date) => {
     return new Date(date).toLocaleDateString('pt-BR',
-    {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-    })
+        {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        })
 }
 
 export const getTag = (post) => {
@@ -20,6 +20,25 @@ export const getImgUrl = (post) => {
 
 export const getTitle = (post) => {
     return RichText.asText(post.data.title)
+}
+
+export const getIntroduction = (post) => {
+    return RichText.asHtml(post.data.introduction) ?? ''
+}
+
+export const getFooter = (post) => {
+    return RichText.asHtml(post.data.footer) ?? ''
+}
+
+export const getContent = (post) => {
+    const content = post.data.content;
+    return content.map(el => {
+        return {
+            ...el,
+            paragraph: RichText.asHtml(el.paragraph),
+            title: RichText.asText(el.title),
+        }
+    });
 }
 
 export const getUid = (post) => {
@@ -36,5 +55,5 @@ export const getPreviewText = (post, flag = false) => {
 
     const previewText = flag ? excerpt.slice(0, 600) : excerpt.slice(0, 200);
 
-   return `${previewText}${toBeContinue}`;
+    return `${previewText}${toBeContinue}`;
 }
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,20 +1,22 @@
 import { GetServerSideProps } from "next";
 import Head from "next/head";
-import { RichText } from "prismic-dom";
-import { getPrismicClient } from "../../services/prismic";
+import { dateToPtbr, getContent, getFooter, getImgUrl, getIntroduction, getTitle } from "../../helpers/util";
+import { api_id, GET_PRISMIC_CLIENT } from "../../services/prismic";
 import styles from './post.module.scss';
 
 interface PostProps {
     post: {
         slug: string;
         title: string;
-        content: string;
-        image: string;
+        introduction: string;
+        content: Array<any>;
+        image_banner: string;
+        footer: string;
         updatedAt: string;
     }
 }
 
-export default function Post ({ post }: PostProps) {
+export default function Post({ post }: PostProps) {
     return (
         <>
             <Head>
@@ -25,38 +27,44 @@ export default function Post ({ post }: PostProps) {
                 <article className={styles.post}>
                     <h1>{post.title}</h1>
                     <time>{post.updatedAt}</time>
-                    <img src={post.image} alt={post.title} />
-                    <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: post.content }} />
+                    <img src={post.image_banner} alt={post.title} />
+                    <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: post.introduction }} />
+
+                    {post.content.map(content => (
+                        <div key={post.title}>
+                            <h3>{post.title}</h3>
+                            <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: content.paragraph }} />
+                        </div>
+                    ))}
+
+                    <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: post.footer }} />
                 </article>
             </main>
         </>
     );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req, params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
     const { slug } = params;
 
-    const prismic = getPrismicClient(req);
+    const prismic = GET_PRISMIC_CLIENT(req);
 
-    const response = await prismic.getByUID<any>('template-post', String(slug), {});
+    const response = await prismic.getByUID<any>(api_id.list_post, String(slug), {});
 
     const post = {
         slug,
-        title: RichText.asText(response.data.title),
-        content: RichText.asHtml(response.data.content),
-        image: response.data.image.url ?? '',
-        updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR',
-        {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-        })
+        title: getTitle(response),
+        introduction: getIntroduction(response),
+        content: getContent(response),
+        image_banner: getImgUrl(response),
+        updatedAt: dateToPtbr(response.last_publication_date),
+        footer: getFooter(response),
     }
 
-        return {
-            props: {
-                post
-            }
+    return {
+        props: {
+            post
+        }
     }
 
 }
