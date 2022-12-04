d18cdb4b1097660bb7fe68a67c36131320ecc0e7
Daniel Soares <danielsoaresf@hotmail.com>
2022-12-04 10:23:24 -0300

    bugfix/ ajuste em build.

---
+++ env.js
@@ -0,0 +1,4 @@
+export const env = {
+    PRISMIC_END_PONT: 'https://lostcode-blog.prismic.io/api/v2',
+    PRISMIC_ACCESS_TOKEN: 'MC5ZNGpkNFJFQUFDZ0FTWFNp.77-9PO-_ve-_vRJ777-977-977-977-9Eu-_vV3vv70g77-9Je-_ve-_ve-_vTR5dO-_vWMM77-9Rnnvv71FcA',
+}
